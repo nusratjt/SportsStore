07d185c7a84b0bb2f024492bfb48f32687f38005
Nusrat <njahan8@centennialcollege.ca>
2020-04-14 01:16:38 -0400

    Completed building core infrastructure of the application

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -38,6 +38,10 @@ namespace SportsStore
             app.UseStaticFiles();
             app.UseMvc(routes => {
                 routes.MapRoute(
+                    name: "pagination",
+                    template: "Products/Page{productPage}",
+                    defaults: new { Controller = "Product", action = "List" });
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Product}/{action=List}/{id?}");
             });
