fd28ea71debbfe890d7ec3d39a29f18fd5dbd75e
Nusrat <njahan8@centennialcollege.ca>
2020-05-01 00:09:30 -0400

    Completed CRUD functionality

--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace SportsStore.Models
 {
     public class Product
     {
-            public int ProductID { get; set; }
-            public string Name { get; set; }
-            public string Description { get; set; }
-            public decimal Price { get; set; }
-            public string Category { get; set; }
+        public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue,
+            ErrorMessage = "Please enter a positive price")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please specify a category")]
+        public string Category { get; set; }
     }
 }
